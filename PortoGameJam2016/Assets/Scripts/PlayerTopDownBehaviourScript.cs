4cd1a9b9076d6d46ba7a78bb16a023b875f292fe
JPonte <joaopcponte@gmail.com>
2016-03-05 16:58:51 +0000

    Fix movement top down.

--- PortoGameJam2016/Assets/Scripts/PlayerTopDownBehaviourScript.cs
+++ PortoGameJam2016/Assets/Scripts/PlayerTopDownBehaviourScript.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class PlayerTopDownBehaviourScript : MonoBehaviour {
 
     //settings for game
-    public float speed = 5.0F;
+    public float speed = 20.0F;
     public float zMovement = 100.0f;
 
     TopDownZPosition currentZPos = TopDownZPosition.Middle;
@@ -22,47 +22,9 @@ public class PlayerTopDownBehaviourScript : MonoBehaviour {
             float translationX = Input.GetAxis("Horizontal");
             float translationZ = Input.GetAxis("Vertical");
 
-            GetComponent<Rigidbody>().AddForce(new Vector3(translationX, 0, 0) * speed);
-
-            if (lastTime > updateCounter)
-            {
-                switch (currentZPos)
-                {
-                    case TopDownZPosition.Top:
-                        if (translationZ < -0.5f)
-                        {
-                            GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, -1) * zMovement);
-                            currentZPos = TopDownZPosition.Middle;
-                            lastTime = 0;
-                        }
-                        break;
-                    case TopDownZPosition.Middle:
-                        if (translationZ < -0.5f)
-                        {
-                            GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, -1) * zMovement);
-                            currentZPos = TopDownZPosition.Down;
-                            lastTime = 0;
-                        }
-                        else if (translationZ > 0.5f)
-                        {
-                            GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, 1) * zMovement);
-                            currentZPos = TopDownZPosition.Top;
-                            lastTime = 0;
-                        }
-                        break;
-                    case TopDownZPosition.Down:
-                        if (translationZ > 0.5f)
-                        {
-                            GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, 1) * zMovement);
-                            currentZPos = TopDownZPosition.Middle;
-                            lastTime = 0;
-                        }
-                        break;
-                }
-            } else
-            {
-                lastTime++;
-            }
+            if(GetComponent<Rigidbody>().velocity.magnitude < 15)
+                GetComponent<Rigidbody>().AddForce(new Vector3(translationX, 0, translationZ) * speed);
+            
         }
     }
 }
