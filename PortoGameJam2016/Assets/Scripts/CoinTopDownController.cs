94a2727aff6524a8f10d285f8b8ebf2a52e83477
Luis Reis <luiscubal@gmail.com>
2016-03-05 16:56:30 +0000

    Coin prefab

---
+++ PortoGameJam2016/Assets/Scripts/CoinTopDownController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinTopDownController : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        gameObject.GetComponentInParent<CoinController>().HandleCollision(other.gameObject);
+    }
+}
