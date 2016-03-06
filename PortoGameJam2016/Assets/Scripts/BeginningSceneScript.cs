bebc77b95dfc83f028f80e96fc7e49551ba1139a
AfonsoBroclan <afrosa2@gmail.com>
2016-03-06 16:22:20 +0000

    beginning scene done

---
+++ PortoGameJam2016/Assets/Scripts/BeginningSceneScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class BeginningSceneScript : MonoBehaviour {
+
+    public Text Text;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(FadeStuff());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator FadeStuff()
+    {
+        yield return new WaitForSeconds(1f);
+        float fadingSeconds = GetComponent<FadingScript>().BeginFade(1);
+        yield return new WaitForSeconds(fadingSeconds);
+        Text.text = "An Hypothetically Award Winning Game";
+        fadingSeconds = GetComponent<FadingScript>().BeginFade(-1);
+        yield return new WaitForSeconds(fadingSeconds);
+
+        yield return new WaitForSeconds(1f);
+        fadingSeconds = GetComponent<FadingScript>().BeginFade(1);
+        yield return new WaitForSeconds(fadingSeconds);
+        Text.text = "TWISTING POWER";
+        fadingSeconds = GetComponent<FadingScript>().BeginFade(-1);
+        yield return new WaitForSeconds(fadingSeconds);
+
+        SceneManager.LoadScene("Level1");
+    }
+}
