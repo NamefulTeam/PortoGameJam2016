113217671acdd19a3e61a22053fd7f60dfb33d66
JPonte <joaopcponte@gmail.com>
2016-03-06 11:14:36 +0000

    Score UI.

--- PortoGameJam2016/Assets/Scripts/PlayerController.cs
+++ PortoGameJam2016/Assets/Scripts/PlayerController.cs
@@ -1,14 +1,17 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
     public int CurrentScore = 0;
     public int lifes = 5;
+    public Text ScoreText;
 
     public void AddScore(int score)
     {
         CurrentScore += score;
+        ScoreText.text = CurrentScore.ToString();
     }
 
     public void OnCollisionEnterChild()
